edf576fed272b3b379c69fd50b466cb81543cd13
Mark <tulsa1990@gmail.com>
2022-10-18 09:14:44 -0600

    Added link to tutorial

--- pubmodule.go
+++ pubmodule.go
@@ -1,5 +1,8 @@
 package pubmodule
 
+// How to Distribute Go Modules
+// https://www.digitalocean.com/community/tutorials/how-to-distribute-go-modules
+
 func Hello() string {
   return "Hello, You!"
 }
